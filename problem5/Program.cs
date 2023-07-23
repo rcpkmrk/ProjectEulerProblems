3ec3be38300d005e8c8611535a057918b00e0fe8
Recep Kamrak <67665665+rcpkmrk@users.noreply.github.com>
2023-07-24 02:40:59 +0300

    Add files via upload

---
+++ problem5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+public class Smallest_Multiple
+{
+    static void Main()
+    {
+        int SmallestNumber = 1;
+        while(SmallestNumber>0)
+        {
+            int value = divisible(SmallestNumber,20);
+            if(value == 1)
+            {
+                Console.WriteLine(SmallestNumber);
+                return;
+            }
+            SmallestNumber++;
+        }    
+    }
+    static int divisible(int number,int to_number)
+    {
+        int n = 1;
+        while (n<=to_number)
+        {
+            if(number % n != 0)
+            {
+                return 0;
+            }
+            n++;
+        }
+        return 1;
+    }
+}
