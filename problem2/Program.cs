6bf034a49e78dd615ad2d5e061a8cce8943bdf79
Recep Kamrak <67665665+rcpkmrk@users.noreply.github.com>
2023-07-24 03:17:02 +0300

    Add files via upload

---
+++ problem2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+internal class Problem_2_Even_Fibonacci_Numbers
+{
+    static void Main()
+    {   
+        int[] fibonacci = new int[102];
+        fibonacci[0]=1;
+        fibonacci[1]=2;
+        int sum = 0;
+        for (int i = 0; i < 100; i++)
+        {            
+            fibonacci[i+2] = fibonacci[i+1]+fibonacci[i];
+        }
+        for(int j=0;j<100;j++)
+        {
+            if(fibonacci[j]<4000000)
+            {
+                if (fibonacci[j] % 2 == 0)
+                {
+                    sum = sum + fibonacci[j];
+                }                
+            }
+            else
+            {
+                break;
+            }            
+        }
+        Console.WriteLine(sum);               
+    }
+}
---
+++ problem3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+//Problem 3: Largest Prime Factor
+long number = 600851475143, largest_prime = 0, i = 3;
+while (i < number)
+{
+    int counter = 2;
+    if (number % i == 0)
+    {        
+        for(int j=2; j<i; j++)
+        {
+            if (i % j != 0) { counter = counter + 1;}
+            else { break; }
+        }        
+    }
+    if(counter == i)
+    {
+        largest_prime = i;
+    } 
+    Console.WriteLine("largest prime: "+largest_prime+" number: "+i);
+    i += 2;
+}
