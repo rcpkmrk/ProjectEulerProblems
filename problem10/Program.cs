adb2d66c3beb46b1101066668f39c2abbe603f9d
Recep Kamrak <67665665+rcpkmrk@users.noreply.github.com>
2023-07-24 03:39:17 +0300

    Add files via upload

---
+++ problem10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+// Problem 10: Summation of Primes
+long number = 3, sum = 2;
+while  (number > 0)
+{
+    int counter = 2;
+    for (int i=2; i<number; i++)
+    {
+        if (number % i != 0) { counter++; }
+        else { break; }
+    }
+    if (counter == number)
+    {
+        sum = sum + number;
+        Console.WriteLine("number: "+number+" sum: "+sum);
+    }
+    if (number > 1999998)
+    {
+        return;
+    }
+    number += 2;
+}
---
+++ problem9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+internal class Problem_9_Special_Pythagorean_Triplet
+{
+    static void Main()
+    {   
+        for(float a=1;a<333;a++)
+        {
+           for(float b=2;b<499;b++)
+        {
+            float equation = (500000-1000*b)/(1000-b);
+            if(equation == a)
+            {
+                if(b>a)
+                {
+                    float c = 1000 - a - b;
+                    float Product_abc = a*b*c;
+                    Console.WriteLine(a);
+                    Console.WriteLine(b);
+                    Console.WriteLine(c);
+                    Console.WriteLine("Product abc is:"+Product_abc);
+                    break; 
+                }                
+            }
+        } 
+        }
+    }
+}
