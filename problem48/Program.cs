2b6c211a360bd88de20e0a15120f50de017cf7d3
Recep Kamrak <67665665+rcpkmrk@users.noreply.github.com>
2025-04-28 19:10:55 +0300

    Add files via upload

---
+++ problem48/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+public class P48_SelfPowers
+{
+    static void Main()
+    {
+        BigInteger sum = 0;
+        for(int i=1; i<=1000; i++)
+        {
+            sum = sum + BigInteger.Pow(i,i);
+        }
+        BigInteger lastTenDigits = sum % BigInteger.Pow(10, 10);
+        Console.WriteLine(lastTenDigits);
+    }
+}
