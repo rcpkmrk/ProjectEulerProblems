50f68b8a186afe51ac0ac91b616bcecd4fc9426e
Recep Kamrak <67665665+rcpkmrk@users.noreply.github.com>
2025-04-29 07:42:59 +0300

    Add files via upload

---
+++ problem40/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class P40_Champernowne_sConstant
+{
+    static void Main()
+    {
+        string decimals = "";
+        for (int i = 0; i < 200000; i++) // just above a million digits
+        {
+            decimals = decimals + i.ToString();
+        }
+
+        int multi = 1;
+        int j = 1;
+        while (j<1000000)
+        {
+            multi = multi*(decimals[j] - '0');
+            j*=10;
+        }
+
+        Console.WriteLine(multi);
+    }
+}
